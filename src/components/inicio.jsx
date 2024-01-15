44e88b9a761657d8e9f67900d9b45838b3febf2e
Franco Hollenstein <hollensteinfranco99@gmail.com>
2024-01-14 22:51:20 -0300

    se agrega grafico

--- src/components/inicio.jsx
+++ src/components/inicio.jsx
@@ -1,8 +1,12 @@
 import React, { Fragment, useEffect, useRef, useState } from 'react';
 import ModalCaja from './Caja/ModalCaja';
 import { useNavigate } from 'react-router-dom';
-import '../css/inicio.css';
+import { Line } from 'react-chartjs-2';
 import moment from 'moment';
+import 'moment/locale/es';
+import 'chart.js/auto';
+import '../css/inicio.css';
+import { Ticks, Tooltip } from 'chart.js/auto';
 
 const Inicio = () => {
     const URL = process.env.REACT_APP_API_URL;
@@ -14,17 +18,120 @@ const Inicio = () => {
     const navigate = useNavigate();
     const montoTotalMesRef = useRef(null);
     const [loading, setLoading] = useState(false);
+    const [ventas, setVentasGrafico] = useState(null);
 
     useEffect(() => {
         consultarEstadoCaja();
     }, [showModal]);
     useEffect(() => {
+
         consultarVentaMes();
+        consultarVentaGrafico();
     }, []);
+    // DATOS GRAFICO LINEAL
+    // Convertir la fecha al formato adecuado y filtrar los datos de los últimos 6 meses
+    const obtenerVentasUltimos6Meses = () => {
+        moment.locale('es');
+
+        const today = moment();
+        const sixMonthsAgo = moment().subtract(6, 'months');
+
+        const ventasTotalesUltimos6Meses = ventas
+            ? ventas
+                .filter((venta) => {
+                    const fechaVenta = moment(venta.fecha_registro, 'DD/MM/YY HH:mm');
+                    return fechaVenta.isBetween(sixMonthsAgo, today, null, '[]');
+                })
+                .reduce((acumulador, venta) => {
+                    const fechaVenta = moment(venta.fecha_registro, 'DD/MM/YY HH:mm');
+                    const mes = fechaVenta.format('MMMM YYYY');
+
+                    if (!acumulador[mes]) {
+                        acumulador[mes] = 0;
+                    }
+
+                    acumulador[mes] += venta.total;
+
+                    return acumulador;
+                }, {})
+            : {};
+
+        // Ordenar las ventas por mes de manera ascendente
+        const ventasOrdenadas = Object.keys(ventasTotalesUltimos6Meses)
+            .sort((a, b) => moment(a, 'MMMM YYYY').valueOf() - moment(b, 'MMMM YYYY').valueOf())
+            .reduce((acumulador, key) => {
+                acumulador[key] = ventasTotalesUltimos6Meses[key];
+                return acumulador;
+            }, {});
+
+        return ventasOrdenadas;
+    };
+    const ventasTotalesUltimos6Meses = obtenerVentasUltimos6Meses();
+
+    // Crear arrays separados para etiquetas y datos
+    const etiquetas = Object.keys(ventasTotalesUltimos6Meses);
+    const datos = Object.values(ventasTotalesUltimos6Meses);
+
+
+    const data = {
+        labels: etiquetas,
+        datasets: [
+            {
+                label: 'Total de Ventas',
+                data: datos,
+                fill: false,
+                borderColor: '#DE4352',
+                backgroundColor: 'transparent',
+            },
+        ],
+    };
+    const options = {
+        plugins: {
+            legend: {
+                labels: {
+                    color: 'white',
+                },
+            },
+            tooltip: {
+                backgroundColor: 'rgba(0, 0, 0, 0.4)',
+                titleColor: 'white',
+                bodyColor: 'white',
+                footerColor: 'white',
+                caretSize: 10,
+                cornerRadius: 8,
+                borderColor: '#DE4352',
+                borderWidth: 1,
+                callbacks: {
+                    beforeBody: function (context) {
+                        return '            ';
+                    },
+                },
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true,
+                grid: {
+                    color: '#6f6f6f'
+                },
+                ticks: {
+                    color: 'white'
+                }
+            },
+            x: {
+                grid: {
+                    display: false,
+                },
+                ticks: {
+                    color: 'white'
+                }
+            },
+        },
+    };
+
 
     const handleClose = () => setShowModal(false);
     const hacerPedido = () => { navigate('/compra-pedido') }
-
     const abrirCaja = () => {
         setAbrirCajaState(estadoCaja === false ? 'abrir' : 'cerrar');
         setShowModal(true);
@@ -87,7 +194,27 @@ const Inicio = () => {
                 });
                 // Suma los totales de las ventas filtradas
                 const totalVentasMesActual = ventasMesActual.reduce((total, venta) => total + venta.total, 0);
-                montoTotalMesRef.current.innerText = new Intl.NumberFormat('es-AR', { style: 'currency', currency: 'ARS' }).format(totalVentasMesActual) ;
+                montoTotalMesRef.current.innerText = new Intl.NumberFormat('es-AR', { style: 'currency', currency: 'ARS' }).format(totalVentasMesActual);
+            } else {
+                console.log('Error en la consulta');
+            }
+        } catch (error) {
+            console.error('Error en la consulta:', error);
+        }
+    }
+    const consultarVentaGrafico = async () => {
+        try {
+            const response = await fetch(`${URL}/venta`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.status === 200) {
+                const ventas = await response.json();
+
+                setVentasGrafico(ventas);
             } else {
                 console.log('Error en la consulta');
             }
@@ -140,6 +267,15 @@ const Inicio = () => {
                                 <p ref={montoTotalMesRef}></p>
                             </div>
                         </div>
+                    </article>
+                    {/* =============================== */}
+                    <article className='w-100 d-flex flex-wrap p-2'>
+
+                        <section className='app-contenedor'>
+                            <article className='contenedor-grafico'>
+                                <Line width='auto' className='grafico' data={data} options={options} />
+                            </article>
+                        </section>
                     </article>
                 </section>
             </section>
